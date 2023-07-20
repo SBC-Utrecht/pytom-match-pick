efadc4d1d620123ab97fcb8ec75d39ad78e2d16d
McHaillet <martenchaillet@gmail.com>
2023-07-20 20:56:51 +0200

    name fixes

--- src/pytom_tm/job.py
+++ src/pytom_tm/job.py
@@ -1,18 +1,30 @@
 import pathlib
+import copy
 from pytom_tm.angles import AVAILABLE_ROTATIONAL_SAMPLING
 
 
 class Job:
-    def __init__(self, subregion, angle_increment):
-
-        self.tomogram = pathlib.Path()
-        self.reference = pathlib.Path()
-        self.mask = pathlib.Path()
+    def __init__(
+            self,
+            job_id: int,
+            tomogram: pathlib.Path,
+            template: pathlib.Path,
+            mask: pathlib.Path,
+            subregion: list[tuple[int]],
+            angle_increment: str
+    ):
+        self.job_id = job_id
+        self.tomogram = tomogram
+        self.reference = template
+        self.mask = mask
 
         self.subregion = subregion
-        self.rotation_file = pathlib.Path(ROTATIONAL_SAMPLING[angle_increment][0])
-        self.n_rotations = ROTATIONAL_SAMPLING[angle_increment][1]
+        self.rotation_file = AVAILABLE_ROTATIONAL_SAMPLING[angle_increment][0]
+        self.n_rotations = AVAILABLE_ROTATIONAL_SAMPLING[angle_increment][1]
         self.rotation_slice = slice(0, self.n_rotations, 1)
 
     def split_rotation_search(self, slices):
         pass
+
+    def copy(self):
+        return copy.deepcopy(self)
