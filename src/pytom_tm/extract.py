64f763b5f2d4aed64c84052a659e36164b0053da
McHaillet <martenchaillet@gmail.com>
2023-09-07 17:47:34 +0200

    simplify search size calculation

--- src/pytom_tm/extract.py
+++ src/pytom_tm/extract.py
@@ -61,7 +61,7 @@ def extract_particles(
         sigma = job.job_stats['std']
         search_space = (
             # wherever the score volume has not been explicitly set to -1 is the size of the search region
-            ((score_volume != -1) * 1).sum() *
+            (score_volume > -1).sum() *
             job.n_rotations
         )
         cut_off = erfcinv((2 * n_false_positives) / search_space) * np.sqrt(2) * sigma
