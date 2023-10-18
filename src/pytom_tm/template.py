33c5a1d0856b0b2eda325a96f9863c45b0aaf4aa
Marten <58044494+McHaillet@users.noreply.github.com>
2023-10-18 11:27:16 +0200

    Update template.py

--- src/pytom_tm/template.py
+++ src/pytom_tm/template.py
@@ -37,7 +37,7 @@ def generate_template_from_map(
         )
 
     if filter_to_resolution is None or filter_to_resolution < (2 * output_spacing):
-        logging.warning(f'Invalid resolution specified, changing to {2 * output_spacing}A')
+        logging.warning(f'Resolution is either not specified or incorrectly specified, changing to {2 * output_spacing}A')
         filter_to_resolution = 2 * output_spacing
 
     # extend volume to the desired output size before applying convolutions!
