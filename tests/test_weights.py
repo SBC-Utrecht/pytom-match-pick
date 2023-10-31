91dfffb2ddd790d816e581564bbb7cf879568423
McHaillet <martenchaillet@gmail.com>
2023-10-31 12:23:15 +0100

    added power_spectrum_profile and profile_to_weighting tests; updated rest of the tests to work with the same input output

--- tests/test_weights.py
+++ tests/test_weights.py
@@ -1,6 +1,7 @@
 import numpy as np
 import unittest
-from pytom_tm.weights import (create_wedge, create_ctf, create_gaussian_band_pass, radial_reduced_grid, radial_average)
+from pytom_tm.weights import (create_wedge, create_ctf, create_gaussian_band_pass, radial_reduced_grid,
+                              radial_average, power_spectrum_profile, profile_to_weighting)
 from pytom_tm.io import write_mrc
 
 
@@ -94,24 +95,36 @@ ACCUMULATED_DOSE = [float(x.strip()) for x in DOSE_FILE.split('\n') if x != '']
 
 class TestWeights(unittest.TestCase):
     def setUp(self):
-        self.volume_shape = (10, 10, 10)
+        self.volume_shape_even = (10, 10, 10)
+        self.volume_shape_uneven = (11, 11, 11)
+        self.volume_shape_irregular = (7, 12, 6)
         self.tilt_angles = list(range(-51, 54, 3))
         self.voxel_size = 3.34
         self.low_pass = 10
         self.high_pass = 50
 
-        self.expected_output_shape = (10, 10, 6)
+        self.reduced_even_shape_3d = (10, 10, 6)
+        self.reduced_even_shape_2d = (10, 6)
+        self.reduced_uneven_shape_3d = (11, 11, 6)
+        self.reduced_uneven_shape_2d = (11, 6)
+        self.reduced_irregular_shape_3d = (7, 12, 6 // 2 + 1)
+        self.reduced_irregular_shape_2d = (7, 12 // 2 + 1)
 
     def test_radial_reduced_grid(self):
-        self.assertEqual(radial_reduced_grid(self.volume_shape).shape, (10, 10, 6),
+        with self.assertRaises(ValueError, msg='Radial reduced grid should raise ValueError if the shape is '
+                                               'not 2- or 3-dimensional.'):
+            radial_reduced_grid((5, ))
+            radial_reduced_grid((5, ) * 4)
+
+        self.assertEqual(radial_reduced_grid(self.volume_shape_even).shape, self.reduced_even_shape_3d,
                          msg='3D radial reduced grid does not have the correct shape')
-        self.assertEqual(radial_reduced_grid(self.volume_shape[:2]).shape, (10, 6),
+        self.assertEqual(radial_reduced_grid(self.volume_shape_even[:2]).shape, self.reduced_even_shape_2d,
                          msg='2D radial reduced grid does not have the correct shape')
 
     def test_band_pass(self):
         with self.assertRaises(ValueError, msg='Bandpass should raise ValueError if both low and high pass are None'):
             create_gaussian_band_pass(
-                self.volume_shape,
+                self.volume_shape_even,
                 self.voxel_size,
                 None,
                 None
@@ -119,24 +132,24 @@ class TestWeights(unittest.TestCase):
         with self.assertRaises(ValueError, msg='Bandpass should raise ValueError if low pass resolution > high pass '
                                                'resolution'):
             create_gaussian_band_pass(
-                self.volume_shape,
+                self.volume_shape_even,
                 self.voxel_size,
                 50,
                 10
             )
-        band_pass = create_gaussian_band_pass(self.volume_shape, self.voxel_size, self.low_pass, self.high_pass)
-        low_pass = create_gaussian_band_pass(self.volume_shape, self.voxel_size, low_pass=self.low_pass)
-        high_pass = create_gaussian_band_pass(self.volume_shape, self.voxel_size, high_pass=self.high_pass)
+        band_pass = create_gaussian_band_pass(self.volume_shape_even, self.voxel_size, self.low_pass, self.high_pass)
+        low_pass = create_gaussian_band_pass(self.volume_shape_even, self.voxel_size, low_pass=self.low_pass)
+        high_pass = create_gaussian_band_pass(self.volume_shape_even, self.voxel_size, high_pass=self.high_pass)
 
-        self.assertEqual(band_pass.shape, self.expected_output_shape,
+        self.assertEqual(band_pass.shape, self.reduced_even_shape_3d,
                          msg='Bandpass filter does not have expected output shape')
         self.assertEqual(band_pass.dtype, np.float64,
                          msg='Bandpass filter does not have expected dtype')
-        self.assertEqual(low_pass.shape, self.expected_output_shape,
+        self.assertEqual(low_pass.shape, self.reduced_even_shape_3d,
                          msg='Low-pass filter does not have expected output shape')
         self.assertEqual(low_pass.dtype, np.float64,
                          msg='Low-pass filter does not have expected dtype')
-        self.assertEqual(high_pass.shape, self.expected_output_shape,
+        self.assertEqual(high_pass.shape, self.reduced_even_shape_3d,
                          msg='High-pass filter does not have expected output shape')
         self.assertEqual(high_pass.dtype, np.float64,
                          msg='High-pass filter does not have expected dtype')
@@ -152,51 +165,51 @@ class TestWeights(unittest.TestCase):
         with self.assertRaises(ValueError, msg='Create wedge should raise ValueError if tilt_angles list does not '
                                                'contain at least two values'):
             create_wedge(
-                self.volume_shape,
+                self.volume_shape_even,
                 [1.],
                 1.
             )
         with self.assertRaises(ValueError, msg='Create wedge should raise ValueError if tilt_angles input is not a '
                                                'list'):
             create_wedge(
-                self.volume_shape,
+                self.volume_shape_even,
                 1.,
                 1.
             )
         with self.assertRaises(ValueError, msg='Create wedge should raise ValueError if voxel_size is smaller or '
                                                'equal to 0'):
             create_wedge(
-                self.volume_shape,
+                self.volume_shape_even,
                 self.tilt_angles,
                 0.
             )
         with self.assertRaises(ValueError, msg='Create wedge should raise ValueError if cut_off_radius is smaller or '
                                                'equal to 0'):
             create_wedge(
-                self.volume_shape,
+                self.volume_shape_even,
                 self.tilt_angles,
                 1.,
                 cut_off_radius=0.
             )
 
         # create test wedges
-        structured_wedge = create_wedge(self.volume_shape, self.tilt_angles, 1., tilt_weighting=True)
-        symmetric_wedge = create_wedge(self.volume_shape, [self.tilt_angles[0], self.tilt_angles[-1]],
+        structured_wedge = create_wedge(self.volume_shape_even, self.tilt_angles, 1., tilt_weighting=True)
+        symmetric_wedge = create_wedge(self.volume_shape_even, [self.tilt_angles[0], self.tilt_angles[-1]],
                                        1., tilt_weighting=False)
-        asymmetric_wedge = create_wedge(self.volume_shape, [self.tilt_angles[0], self.tilt_angles[-2]],
+        asymmetric_wedge = create_wedge(self.volume_shape_even, [self.tilt_angles[0], self.tilt_angles[-2]],
                                         1., tilt_weighting=False)
 
-        self.assertEqual(structured_wedge.shape, self.expected_output_shape,
+        self.assertEqual(structured_wedge.shape, self.reduced_even_shape_3d,
                          msg='Structured wedge does not have expected output shape')
         self.assertEqual(structured_wedge.dtype, np.float32,
                          msg='Structured wedge does not have expected dtype')
 
-        self.assertEqual(symmetric_wedge.shape, self.expected_output_shape,
+        self.assertEqual(symmetric_wedge.shape, self.reduced_even_shape_3d,
                          msg='Symmetric wedge does not have expected output shape')
         self.assertEqual(symmetric_wedge.dtype, np.float32,
                          msg='Symmetric wedge does not have expected dtype')
 
-        self.assertEqual(asymmetric_wedge.shape, self.expected_output_shape,
+        self.assertEqual(asymmetric_wedge.shape, self.reduced_even_shape_3d,
                          msg='Asymmetric wedge does not have expected output shape')
         self.assertEqual(asymmetric_wedge.dtype, np.float32,
                          msg='Asymmetric wedge does not have expected dtype')
@@ -204,28 +217,29 @@ class TestWeights(unittest.TestCase):
         self.assertTrue(np.sum((symmetric_wedge != asymmetric_wedge) * 1) != 0,
                         msg='Symmetric and asymmetric wedge should be different!')
 
-        structured_wedge = create_wedge(self.volume_shape, self.tilt_angles, self.voxel_size, tilt_weighting=True,
+        structured_wedge = create_wedge(self.volume_shape_even, self.tilt_angles, self.voxel_size, tilt_weighting=True,
                                         cut_off_radius=1., low_pass=self.low_pass, high_pass=self.high_pass)
-        self.assertEqual(structured_wedge.shape, self.expected_output_shape,
+        self.assertEqual(structured_wedge.shape, self.reduced_even_shape_3d,
                          msg='Wedge with band-pass does not have expected output shape')
         self.assertEqual(structured_wedge.dtype, np.float32,
                          msg='Wedge with band-pass does not have expected dtype')
 
-    def test_3d_ctf(self):
-        weights = create_wedge((40,) * 3, self.tilt_angles, self.voxel_size * 3, tilt_weighting=True, low_pass=40,
+        weights = create_wedge(self.volume_shape_even, self.tilt_angles, self.voxel_size * 3,
+                               tilt_weighting=True, low_pass=40,
                                accumulated_dose_per_tilt=ACCUMULATED_DOSE,
                                ctf_params_per_tilt=CTF_PARAMS)
-        self.assertEqual(weights.shape, (40, 40, 21),
+        self.assertEqual(weights.shape, self.reduced_even_shape_3d,
                          msg='3D CTF does not have the correct reduced fourier shape.')
-        weights = create_wedge((41,) * 3, self.tilt_angles, self.voxel_size * 3, tilt_weighting=True, low_pass=40,
+        weights = create_wedge(self.volume_shape_uneven, self.tilt_angles, self.voxel_size * 3,
+                               tilt_weighting=True, low_pass=40,
                                accumulated_dose_per_tilt=ACCUMULATED_DOSE,
                                ctf_params_per_tilt=CTF_PARAMS)
-        self.assertEqual(weights.shape, (41, 41, 21),
+        self.assertEqual(weights.shape, self.reduced_uneven_shape_3d,
                          msg='3D CTF does not have the correct reduced fourier shape.')
 
     def test_ctf(self):
         ctf_raw = create_ctf(
-            self.volume_shape,
+            self.volume_shape_even,
             self.voxel_size * 1E-10,
             3E-6,
             0.08,
@@ -233,7 +247,7 @@ class TestWeights(unittest.TestCase):
             2.7E-3
         )
         ctf_cut = create_ctf(
-            self.volume_shape,
+            self.volume_shape_even,
             self.voxel_size * 1E-10,
             3E-6,
             0.08,
@@ -241,7 +255,7 @@ class TestWeights(unittest.TestCase):
             2.7E-3,
             cut_after_first_zero=True
         )
-        self.assertEqual(ctf_raw.shape, self.expected_output_shape,
+        self.assertEqual(ctf_raw.shape, self.reduced_even_shape_3d,
                          msg='CTF does not have expected output shape')
         self.assertTrue(np.sum((ctf_raw != ctf_cut) * 1) != 0,
                         msg='CTF should be different when cutting it off after the first zero crossing')
@@ -256,6 +270,34 @@ class TestWeights(unittest.TestCase):
             radial_average(
                 np.zeros((100, 50))
             )
+
+    def test_power_spectrum_profile(self):
+        with self.assertRaises(ValueError, msg='Power spectrum profile should raise ValueError if input image is '
+                                               'not 2- or 3-dimensional.'):
+            power_spectrum_profile(np.zeros(5))
+            power_spectrum_profile(np.zeros((5, ) * 4))
+        profile = power_spectrum_profile(np.zeros(self.volume_shape_irregular))
+        self.assertEqual(profile.shape, (max(self.volume_shape_irregular) // 2 + 1, ),
+                         msg='Power spectrum profile output shape should be a 1-dimensional array with '
+                             'length equal to max(input_shape) // 2 + 1, corresponding to largest sampling component '
+                             'in Fourier space.')
+
+    def test_profile_to_weighting(self):
+        with self.assertRaises(ValueError, msg='Profile to weighting should raise a ValueError if the profile is not '
+                                               '1-dimensional.'):
+            profile_to_weighting(np.zeros((5, 5)), (5, 5))
+        with self.assertRaises(ValueError, msg='Profile to weighting should raise a ValueError if the output shape '
+                                               'for the weighting is not 2- or 3-dimensional.'):
+            profile_to_weighting(np.zeros(5), (5, ))
+            profile_to_weighting(np.zeros(5), (5,) * 4)
+
+        profile = power_spectrum_profile(np.zeros(self.volume_shape_irregular))
+        self.assertEqual(profile_to_weighting(profile, self.volume_shape_irregular).shape,
+                         self.reduced_irregular_shape_3d,
+                         msg='Profile to weighting should return 3D Fourier reduced array.')
+        self.assertEqual(profile_to_weighting(profile, self.volume_shape_irregular[:2]).shape,
+                         self.reduced_irregular_shape_2d,
+                         msg='Profile to weighting should return 2D Fourier reduced array.')
 
 
 if __name__ == '__main__':
