39cb96b4071a7324dbbc72adc4bf20408941aaa9
McHaillet <martenchaillet@gmail.com>
2023-12-14 16:57:27 +0100

    add gold marker masking

--- src/pytom_tm/extract.py
+++ src/pytom_tm/extract.py
@@ -14,27 +14,73 @@ from scipy.special import erfcinv
 from tqdm import tqdm
 
 
-def detect_blobs(
+def predict_gold_marker_mask(
         volume: npt.NDArray[float],
         voxel_size: float,
         blob_size: float,
-        threshold: float = 1.
 ) -> npt.NDArray[int]:
+    # gold marker radius in number of voxels
     blob_radius = (blob_size / voxel_size) / 2
+
+    # Laplacian of Gaussian (LoG) filter where sigma is gaussian width corresponding to object size
     sigma = blob_radius / np.sqrt(3)
-    filtered = ndimage.gaussian_laplace((volume - volume.mean()) / volume.std(), sigma)
-    return ((filtered > threshold) * 1).astype(int)
+    log = ndimage.gaussian_laplace(volume, sigma)
+
+    # fudge factor for the number of iterations for binary opening and dilation
+    iters = int(blob_radius / (5 / 3))  # 5/3 is a fudge ratio
+
+    mask = ndimage.binary_opening(  # binary opening keeps objects that consist of plenty connected components
+        (log > log.std() * 3) * 1,  # LoG is threshold with 3x the standard deviation
+        iterations=iters,  # iterations should be more for smaller voxel spacings
+        structure=ndimage.generate_binary_structure(
+            rank=3, connectivity=1
+        )
+    )  # dilate strongly to ensure gold is fully covered
+    return ndimage.binary_dilation(
+        mask,
+        iterations=iters + 2,
+        structure=ndimage.generate_binary_structure(rank=3, connectivity=2),
+    )
 
 
 def extract_particles(
         job: TMJob,
         particle_radius_px: int,
         n_particles: int,
-        cut_off: Optional[float] = None,
         n_false_positives: int = 1,
+        cut_off: Optional[float] = None,
+        tophat_filter_cut_off: Optional[float] = None,
+        gold_marker_diameter: Optional[float] = None,
         tomogram_mask_path: Optional[pathlib.Path] = None,
-        tophat_filter: bool = False,
 ) -> tuple[pd.DataFrame, list[float, ...]]:
+    """
+    Parameters
+    ----------
+    job: pytom_tm.tmjob.TMJob
+        template matching job to annotate particles from
+    particle_radius_px: int
+        particle radius to remove peaks with after a score has been annotated
+    n_particles: int
+        maximum number of particles to extract
+    n_false_positives: int
+        tune the number of false positives to be included for automated error function cut-off estimation: default is 1;
+        lowering the value up to zero reduces sensitivity; increasing the value above 1 increases sensitivity,
+        it should roughly relate to the number of false positives (i.e. 500 expects 500 FPs)
+    cut_off: Optional[float]
+        manually override the automated score cut-off estimation, value between 0 and 1
+    tophat_filter_cut_off: Optional[float]
+        float between 0 and 1 to tune the specificity: default is 0.1; value of 1 removes everything, value of 0
+        includes everything
+    gold_marker_diameter: Optional[float]
+        average diameter of gold markers in angstrom
+    tomogram_mask_path: Optional[pathlib.Path]
+        path to a tomographic binary mask for extraction
+
+    Returns
+    -------
+    dataframe, scores: tuple[pd.DataFrame, list[float, ...]]
+        dataframe with annotations that can be written out as a STAR file and a list of the selected scores
+    """
 
     score_volume = read_mrc(job.output_dir.joinpath(f'{job.tomo_id}_scores.mrc'))
     angle_volume = read_mrc(job.output_dir.joinpath(f'{job.tomo_id}_angles.mrc'))
@@ -43,7 +89,15 @@ def extract_particles(
         sort_angles=version.parse(job.pytom_tm_version_number) > version.parse('0.3.0')
     )
 
-    if tophat_filter:  # constrain the extraction with a tophat filter
+    if gold_marker_diameter is not None:
+        mask = predict_gold_marker_mask(
+            read_mrc(job.tomograms),  # the gold mark mask is created on the tomogram
+            job.voxel_size,
+            gold_marker_diameter,
+        )  # zero all elements where gold beads are predicted
+        score_volume[mask == 1] = 0
+
+    if tophat_filter_cut_off is not None:  # constrain the extraction with a tophat filter
         layer1 = ndimage.white_tophat(
             score_volume,
             structure=ndimage.generate_binary_structure(
@@ -58,18 +112,10 @@ def extract_particles(
                 connectivity=2
             )
         )
-        flt = layer1 * layer2
-        flt = (flt - flt.min()) / (flt.max() - flt.min())  # normalise
-        tophat_cut_off = 0.1  # this is a user parameter
-        flt[flt < tophat_cut_off] = 0  # threshold tophat
-
-    # mask edges of score volume
-    score_volume[0: particle_radius_px, :, :] = 0
-    score_volume[:, 0: particle_radius_px, :] = 0
-    score_volume[:, :, 0: particle_radius_px] = 0
-    score_volume[-particle_radius_px:, :, :] = 0
-    score_volume[:, -particle_radius_px:, :] = 0
-    score_volume[:, :, -particle_radius_px:] = 0
+        tophat_filter = layer1 * layer2
+        tophat_filter = (tophat_filter - tophat_filter.min()) / (tophat_filter.max() - tophat_filter.min())
+        # zero places where the tophat filter has too small values
+        score_volume[tophat_filter < tophat_filter_cut_off] = 0  # threshold tophat
 
     # apply tomogram mask if provided
     if tomogram_mask_path is not None:
@@ -79,6 +125,14 @@ def extract_particles(
             job.search_origin[2]: job.search_origin[2] + job.search_size[2]
         ]
         score_volume[tomogram_mask <= 0] = 0
+
+    # mask edges of score volume
+    score_volume[0: particle_radius_px, :, :] = 0
+    score_volume[:, 0: particle_radius_px, :] = 0
+    score_volume[:, :, 0: particle_radius_px] = 0
+    score_volume[-particle_radius_px:, :, :] = 0
+    score_volume[:, -particle_radius_px:, :] = 0
+    score_volume[:, :, -particle_radius_px:] = 0
 
     sigma = job.job_stats['std']
     if cut_off is None:
