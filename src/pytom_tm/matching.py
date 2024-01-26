be72048a079dcb264a570a1e49aee5a134ff5637
McHaillet <martenchaillet@gmail.com>
2024-01-26 15:10:41 +0100

    replace with lowest value and take sqrt before return statement

--- src/pytom_tm/matching.py
+++ src/pytom_tm/matching.py
@@ -198,12 +198,13 @@ def std_under_mask_convolution(
     std convolution of volume and mask
     """
     volume_rft = rfftn(volume) if volume_rft is None else volume_rft
-    std_v = cp.sqrt(
+    std_v = (
             mean_under_mask_convolution(rfftn(volume ** 2), padded_mask, mask_weight) -
             mean_under_mask_convolution(volume_rft, padded_mask, mask_weight) ** 2
     )
     # set extremely low values to 1 to prevent division by 0 and make sure that scores in those areas of the
-    std_v[std_v <= cp.float32(1e-18)] = 1  # tomogram are not inflated
+    std_v[std_v <= cp.float32(1e-18)] = 1e-18  # tomogram are not inflated
+    std_v = cp.sqrt(std_v)
     return std_v
 
 
