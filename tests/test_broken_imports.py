dd99861db18157d9fe86a83e87d3dd5943721d67
Sander Roet <sanderroet@hotmail.com>
2024-02-27 17:48:16 +0100

    try using importlib reload instead

--- tests/test_broken_imports.py
+++ tests/test_broken_imports.py
@@ -1,7 +1,14 @@
 # No imports of pytom_tm outside of the methods
 import unittest
+from importlib import reload
 # Mock out installed dependencies
 orig_import = __import__
+try:
+    import matplotlib
+    import seaborn
+    skip_plotting = False
+except ImportError:
+    skip_plotting = True
 
 def module_not_found_mock(missing_name):
     def import_mock(name, *args):
@@ -19,27 +26,36 @@ matplotlib_not_found = module_not_found_mock('matplotlib')
 seaborn_not_found = module_not_found_mock('seaborn')
 
 class TestMissingDependencies(unittest.TestCase):
+
     def test_missing_cupy(self):
+        # assert working import
+        with self.assertNoLogs(level='WARNING'):
+            import pytom_tm
         cupy_not_found = module_not_found_mock('cupy')
         # Test missing cupy
         with unittest.mock.patch('builtins.__import__', side_effect=cupy_not_found):
             with self.assertLogs(level='WARNING') as cm:
-                import pytom_tm
+                reload(pytom_tm)
             self.assertEqual(len(cm.output), 1)
             self.assertIn("cupy installation not found or not functional", cm.output[0])
 
     def test_broken_cupy(self):
+        # assert working import
+        with self.assertNoLogs(level='WARNING'):
+            import pytom_tm
         # Test cupy ImportError
         with unittest.mock.patch('builtins.__import__', side_effect=cupy_import_error_mock):
             with self.assertLogs(level='WARNING') as cm:
-                import pytom_tm
+                reload(pytom_tm)
             self.assertEqual(len(cm.output), 1)
             self.assertIn("cupy installation not found or not functional", cm.output[0])
 
     def test_missing_matplotlib(self):
+        # assert working import
+        import pytom_tm
         with unittest.mock.patch('builtins.__import__', side_effect=matplotlib_not_found):
             # check if we can still import pytom_tm
-            import pytom_tm
+            reload(pytom_tm)
             # check if plotting is indeed disabled
             self.assertFalse(pytom_tm.template.plotting_available)
             self.assertFalse(pytom_tm.extract.plotting_available)
@@ -48,9 +64,11 @@ class TestMissingDependencies(unittest.TestCase):
                 import pytom_tm.plotting
 
     def test_missing_seaborn(self):
+        # assert working import
+        import pytom_tm
         with unittest.mock.patch('builtins.__import__', side_effect=seaborn_not_found):
             # check if we can still import pytom_tm
-            import pytom_tm
+            reload(pytom_tm)
             # check if plotting is indeed disabled
             self.assertFalse(pytom_tm.template.plotting_available)
             self.assertFalse(pytom_tm.extract.plotting_available)
