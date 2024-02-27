4b522cfbf21682123ab1c80619efe5ae2a8be4b4
Sander Roet <sanderroet@hotmail.com>
2024-02-27 17:42:43 +0100

    actually go for logging instead of raising a warning

--- src/pytom_tm/__init__.py
+++ src/pytom_tm/__init__.py
@@ -1,7 +1,9 @@
+import logging
 from importlib import metadata
 __version__ = metadata.version('pytom-match-pick')
+
 
 try:
     import cupy
 except (ModuleNotFoundError, ImportError):
-    raise ImportWarning('Error for template matching: cupy installation not found or not functional.')
+     logging.warning('Error for template matching: cupy installation not found or not functional.')
