2b54ddf47b0680f1ac0df8202cca92fa32c06b52
Sander Roet <sanderroet@hotmail.com>
2024-02-28 14:00:13 +0100

    Update src/pytom_tm/__init__.py
    Co-authored-by: Marten <58044494+McHaillet@users.noreply.github.com>

--- src/pytom_tm/__init__.py
+++ src/pytom_tm/__init__.py
@@ -6,4 +6,4 @@ __version__ = metadata.version('pytom-match-pick')
 try:
     import cupy
 except (ModuleNotFoundError, ImportError):
-     logging.warning('Error for template matching: cupy installation not found or not functional.')
+    logging.warning('Error for template matching: cupy installation not found or not functional.')
