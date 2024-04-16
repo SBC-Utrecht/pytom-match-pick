0ba32b730646b7de2f72f6bf1052e269e14c2281
McHaillet <martenchaillet@gmail.com>
2024-04-16 09:10:23 +0200

    remove deprecaiton warning of assertEquals

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,7 +8,7 @@ class TestUtils(unittest.TestCase):
         x_offset_um = 200 * 13.79 * 1e-4
         z_offset_um = 100 * 13.79 * 1e-4
         defocus_offsets = get_defocus_offsets(x_offset_um, z_offset_um, tilt_angles)
-        self.assertEquals(
+        self.assertEqual(
             len(defocus_offsets),
             len(tilt_angles),
             msg='get_defocus_offsets did not return a list with the same length as the number of tilt_angles'
