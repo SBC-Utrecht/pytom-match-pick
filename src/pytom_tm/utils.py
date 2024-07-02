b3bfe6c6109e514eec1e3126a003af85d5df1dc3
McHaillet <martenchaillet@gmail.com>
2024-07-02 09:55:22 +0200

    feature: add inversion option and set as default

--- src/pytom_tm/utils.py
+++ src/pytom_tm/utils.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import numpy as np
+import numpy.typing as npt
 
 
 class mute_stdout_stderr(object):
@@ -22,18 +23,32 @@ class mute_stdout_stderr(object):
 
 
 def get_defocus_offsets(
-        patch_center_x: float,
-        patch_center_z: float,
-        tilt_angles: list[float, ...],
-        angles_in_degrees: bool = True
-) -> list[float]:
+    patch_center_x: float,
+    patch_center_z: float,
+    tilt_angles: list[float, ...],
+    angles_in_degrees: bool = True,
+    invert: bool = True,
+) -> npt.NDArray[float]:
     n_tilts = len(tilt_angles)
-    x_centers = np.array([patch_center_x, ] * n_tilts)
-    z_centers = np.array([patch_center_z, ] * n_tilts)
+    x_centers = np.array(
+        [
+            patch_center_x,
+        ]
+        * n_tilts
+    )
+    z_centers = np.array(
+        [
+            patch_center_z,
+        ]
+        * n_tilts
+    )
     ta_array = np.array(tilt_angles)
     # if upside_down:  # invert the tilt_angles if tomogram is reconstructed upside down? but then template should
     #   ta_array *= -1  # also be mirrored
     if angles_in_degrees:
         ta_array = np.deg2rad(ta_array)
-    z_offsets = list(z_centers * np.cos(ta_array) + x_centers * np.sin(ta_array))
-    return z_offsets
+    z_offsets = z_centers * np.cos(ta_array) + x_centers * np.sin(ta_array)
+    if invert:
+        return z_offsets * -1
+    else:
+        return z_offsets
