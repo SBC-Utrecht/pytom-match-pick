7c4f55b6308f0b29bafeb3fb9ada39550e4388ba
McHaillet <martenchaillet@gmail.com>
2024-08-09 16:32:52 +0200

    add docstrings and set default handedness based on literature

--- src/pytom_tm/utils.py
+++ src/pytom_tm/utils.py
@@ -27,8 +27,34 @@ def get_defocus_offsets(
     patch_center_z: float,
     tilt_angles: list[float, ...],
     angles_in_degrees: bool = True,
-    invert: bool = True,
+    invert_handedness: bool = False,
 ) -> npt.NDArray[float]:
+    """Calculate the defocus offsets for a subvolume
+    based on the tilt geometry.
+
+    I used the definition from Pyle & Zianetti (https://doi.org/10.1042/BCJ20200715)
+    for the default setting of the defocus handedness. It assumes the defocus
+    increases for positive tilt angles on the right side of the sample (positive X
+    coordinate relative to the center).
+
+    Parameters
+    ----------
+    patch_center_x: float
+        x center of subvolume relative to tomogram center
+    patch_center_z: float
+        z center of subvolume relative to tomogram center
+    tilt_angles: list[float, ...]
+        list of tilt angles
+    angles_in_degrees: bool, default True
+        whether tilt angles are in degrees or radians
+    invert_handedness: bool, default False
+        invert defocus handedness geometry
+
+    Returns
+    -------
+    z_offsets: npt.NDArray[float]
+        an array of defocus offsets for each tilt angle
+    """
     n_tilts = len(tilt_angles)
     x_centers = np.array(
         [
@@ -45,8 +71,7 @@ def get_defocus_offsets(
     ta_array = np.array(tilt_angles)
     if angles_in_degrees:
         ta_array = np.deg2rad(ta_array)
+    if invert_handedness:
+        ta_array *= -1
     z_offsets = z_centers * np.cos(ta_array) + x_centers * np.sin(ta_array)
-    if invert:
-        return z_offsets * -1
-    else:
-        return z_offsets
+    return z_offsets
