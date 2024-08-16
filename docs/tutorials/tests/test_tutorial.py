f0b86c61f98b967afe33aa4895f773824b19af98
Sander Roet <sanderroet@hotmail.com>
2024-08-16 15:11:44 +0200

    elaborate more on the codeblocks

--- docs/tutorials/tests/test_tutorial.py
+++ docs/tutorials/tests/test_tutorial.py
@@ -6,6 +6,10 @@ from mdextractor import extract_md_blocks
 print("Doing tutorial tests")
 lines = "".join(open("Tutorial.md").readlines())
 blocks = extract_md_blocks(lines)
+n_blocks = len(blocks)
+print(f"Found {n_blocks} code blocks")
+if n_blocks == 0:
+    raise ValueError("Did not find any code blocks")
 for block in blocks:
     if block.split()[0].endswith(".py"):
         print(f"Running: {block}")
