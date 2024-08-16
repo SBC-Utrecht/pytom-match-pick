e07addb275b732690e93c0d5cbae98e3f796b702
Sander Roet <sanderroet@hotmail.com>
2024-08-16 15:32:58 +0200

    try to strip out extra code-block typing

--- docs/tutorials/tests/test_tutorial.py
+++ docs/tutorials/tests/test_tutorial.py
@@ -2,7 +2,6 @@
 import subprocess
 from mdextractor import extract_md_blocks
 
-
 print("Doing tutorial tests")
 lines = "".join(open("Tutorial.md").readlines())
 blocks = extract_md_blocks(lines)
@@ -11,6 +10,8 @@ print(f"Found {n_blocks} code blocks")
 if n_blocks == 0:
     raise ValueError("Did not find any code blocks")
 for block in blocks:
+    # strip out extra typing
+    block = block.strip('bash')
     # DEBUG: TODO: REMOVE
     print(f"{block.split()=}")
     if block.split()[0].endswith(".py"):
