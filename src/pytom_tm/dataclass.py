ade9314494eb261170b25f0991e87482fad805b4
Sander Roet <sanderroet@hotmail.com>
2025-08-08 15:22:58 +0200

    update docstring of the dataclass to the correct units

--- src/pytom_tm/dataclass.py
+++ src/pytom_tm/dataclass.py
@@ -9,13 +9,13 @@ class CtfData(JsonSerializable):
     Attributes
     ----------
     defocus: float
-        defocus in µm
+        defocus in m
     amplitude_contrast: float
         fraction of amplitude contrast between 0 and 1
     voltage: float
-        voltage in KeV
+        voltage in eV
     spherical_aberration: float
-        spherical aberration in mm
+        spherical aberration in m
     phase_shift_deg: float, default 0.0
         phase shift for phase plates in degree
     flip_phase: bool, default False
