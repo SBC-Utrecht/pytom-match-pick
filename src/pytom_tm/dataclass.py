4220287c7d1a5be5d85bd295072efc20e86c9604
Sander Roet <sanderroet@hotmail.com>
2025-08-08 18:28:54 +0200

    remove commented out next possible dataclasses

--- src/pytom_tm/dataclass.py
+++ src/pytom_tm/dataclass.py
@@ -28,27 +28,3 @@ class CtfData(JsonSerializable):
     spherical_aberration: float
     phase_shift_deg: float = 0.0
     flip_phase: bool = False
-
-
-# @dataclass(kw_only=True)
-# class TiltSeriesMetaData:
-#    tilt_angles: list[float]
-#    dose_accumulation: list[float] | None = None
-#    ctf_param: list[CtfData] | None = None
-#    per_tilt_weighting: bool = False
-#
-#    def __post_init__(self):
-#        """Check that if something is given it has the same length as tilt_angles"""
-#        expected = len(self.tilt_angles)
-#        for key, val in asdict(self).items():
-#            if type(val) is list and len(val) != expected:
-#                raise ValueError(
-#                    f"{key} should have the same lenght as the list of "
-#                    f"tilt_angles. Expected {expected}, found {len(val)}"
-#                )
-#
-#
-# @dataclass(kw_only=True)
-# class Relion5MetaData(TiltSeriesMetaData):
-#    binning: float
-#    tilt_series_pixel_size: float
