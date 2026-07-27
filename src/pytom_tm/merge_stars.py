6855b6854374ed2885afdb2c98bc0ddd5cd927a8
Sander Roet <sanderroet@hotmail.com>
2026-07-27 08:30:31 +0200

    fix merge stars

--- src/pytom_tm/merge_stars.py
+++ src/pytom_tm/merge_stars.py
@@ -24,7 +24,7 @@ def merge_stars(
         concatenating all the starfiles together (default)
     """
     # Make sure all paths are absolute and unique
-    files = set(f.resolve() for f in input_star_files)
+    files = {f.resolve() for f in input_star_files}
 
     # Warn if we end up with less files (due to symlinks pointing to the same thing
     # or the user giving the same star file multiple times)
